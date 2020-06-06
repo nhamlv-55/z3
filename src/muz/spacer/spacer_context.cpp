d51b8a7d51bd88711ea26e5efcfd475724c1ec67
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-06-05 21:31:44 -0400

    fix dump_json still running even though it shouldnt

--- src/muz/spacer/spacer_context.cpp
+++ src/muz/spacer/spacer_context.cpp
@@ -3357,11 +3357,11 @@ bool context::is_reachable(pob &n)
 
 void context::dump_json()
 {
-    if (!m_params.spacer_print_json().is_null()) {
-        std::ofstream of;
-        of.open(m_params.spacer_print_json().bare_str());
-        m_json_marshaller.marshal(of);
-        of.close();
+    if (m_params.spacer_print_json()!=symbol("null")) {
+      std::ofstream of;
+      of.open(m_params.spacer_print_json().bare_str());
+      m_json_marshaller.marshal(of);
+      of.close();
     }
 }
 
@@ -4021,7 +4021,7 @@ void context::add_constraint (expr *c, unsigned level)
 }
 
 void context::new_lemma_eh(pred_transformer &pt, lemma *lem) {
-    if (!m_params.spacer_print_json().is_null())
+    if (m_params.spacer_print_json()!=symbol("null"))
         m_json_marshaller.register_lemma(lem);
     bool handle=false;
     for (unsigned i = 0; i < m_callbacks.size(); i++) {
@@ -4045,7 +4045,7 @@ void context::new_lemma_eh(pred_transformer &pt, lemma *lem) {
 }
 
 void context::new_pob_eh(pob *p) {
-    if (!m_params.spacer_print_json().is_null())
+    if (m_params.spacer_print_json()!=symbol("null"))
         m_json_marshaller.register_pob(p);
 }
 
